7b619860d6b123433d14ab7bcddd748f837da761
Charles Collombert <charles.collombert@insa-rennes.fr>
2015-01-27 21:24:19 +0100

    Diagrammes séquence

---
+++ Modele-Controleur/Categorie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modele_Controleur
+{
+    public enum Categorie
+    {
+        REGISTRE_MATRICULE,
+        ACTE_NAISSANCE,
+        ACTE_MARIAGE,
+        ACTE_DECES,
+        TABLE_REGISTRE_MATRICULE,
+    }
+}
