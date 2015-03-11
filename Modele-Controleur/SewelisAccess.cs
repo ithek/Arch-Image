8481cb086c457d7a7e727364b034ceb04512ba77
ithek <kev.thek@gmail.com>
2015-03-11 20:55:08 +0100

    Sewelis

--- Modele-Controleur/SewelisAccess.cs
+++ Modele-Controleur/SewelisAccess.cs
@@ -2,11 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 
 namespace Modele_Controleur
 {
     public class SewelisAccess
     {
+        private WebClient webClient;
+        private const String sewelisURL = "http://149.91.83.183/";
+
+        public SewelisAccess()
+        {
+            webClient = new WebClient();
+        }
+
+        public void creerStore(String nom)
+        {
+            webClient.DownloadString(sewelisURL + "createStore?userKey=123&filename=" + nom);
+        }
+
+        public void chargerRdf(String chemin)
+        {
+            webClient.DownloadString(sewelisURL + "importRdf?userKey=123&storeId=1&base=archimage&filename=" + chemin);
+        }
+
+        public void ajouterPOI(POICreationData poi)
+        {
+
+        }
+
+        public List<Document> GetListDocs(Categorie categorie)
+        {
+            string response = webClient.DownloadString(sewelisURL + "resultsOfStatement?userKey=123&storeId=1&statement=[a <file:///home/kevin/sewelis/img/FRAD035_1R_01901/FRAD035_1R_01901a/RegistreMatricule>]");
+            List<Document> documents = new List<Document>();
+            /*XmlDocument doc = new XmlDocument();
+            doc.LoadXml(response);
+
+            XmlNodeList nodes = doc.GetElementsByTagName("URI");
+
+           
+
+            foreach (XmlNode node in nodes)
+            {
+                string url = node.InnerText;
+                string[] tokens = url.Split(new string[] { "/" }, StringSplitOptions.None);
+
+                Document document = new Document(Categorie.REGISTRE_MATRICULE, tokens[tokens.Length - 1], 0, 0);
+                documents.Add(document);
+            }*/
+
+            return documents;
+        }
+
         /**
          * Renvoie le document n°position
          */
